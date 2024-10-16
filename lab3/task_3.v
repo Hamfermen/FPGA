d097c25d3743629440a3daf576cc5b44357d9a7a
Hamferman <kalash0410@gmail.com>
2024-10-16 19:59:31 +0300

    Task_3_done

--- lab3/task_3.v
+++ lab3/task_3.v
@@ -1,9 +1,18 @@
 
 
 module task_3(
-	input a, b, c, d,
-    output q );
+	input[3:0] a, b, c, d, e,
+    output reg[3:0] q );
 
-assign q = b & (c | d) | a & (c | d);
+always @(c) begin
+case(c)
+        4'h0: q <= b;
+        4'h1: q <= e;
+        4'h2: q <= a;
+        4'h3: q <= d;
+
+        default: q <= 'hf;
+endcase
+end
 
 endmodule
--- lab3/task_3_tb.v
+++ lab3/task_3_tb.v
@@ -2,47 +2,89 @@
 // `include "task_1.v"
 
 module task_3_tb;
-	reg a=0, b=0, c=0, d=0;
+	
+	reg[3:0] a, b, c, d, e;
 
-    wire q;
+    wire[3:0] q;
 
-	task_1 tsk0
+	task_3 tsk0
 	(
 		.a(a),
         .b(b),
         .c(c),
         .d(d),
+		.e(e),
 		.q(q)
 	);
 
+
+	initial
+	begin
+	$display ("###################################################");
+	$display ("Start TestBench");
+	end
+
+	 initial 
+	begin
+		$dumpfile ("task_3.vcd");
+		$dumpvars(0, task_3_tb);
+	end
+
+	event second;
+	event third;
+
 	initial begin
-		$dumpfile("wave.vcd");		// create a VCD waveform dump called "wave.vcd"
-		$dumpvars(0, task_3_tb);		// dump variable changes in the testbench
-									// and all modules under it
-		#5 a=0; b=0; c=0; d=0;
-		#5;a=0; b=0; c=0; d=1;
-		#5;a=0; b=0; c=1; d=0;
-		#5;a=0; b=0; c=1; d=1;
-		#5;a=0; b=1; c=0; d=0;
-		#5;a=0; b=1; c=0; d=1;
-		#5;a=0; b=1; c=1; d=0;
-		#5;a=0; b=1; c=1; d=1;
-		#5;a=1; b=0; c=0; d=0;
-		#5;a=1; b=0; c=0; d=1;
-		#5;a=1; b=0; c=1; d=0;
-		#5;a=1; b=0; c=1; d=1;
-		#5;a=1; b=1; c=0; d=0;
-		#5;a=1; b=1; c=0; d=1;
-		#5;a=1; b=1; c=1; d=0;
-		#5;a=1; b=1; c=1; d=1;
-		#5;a=0; b=0; c=0; d=0;
-		#5;a=0; b=0; c=0; d=1;
-        #5
+
+		a = 'ha; 
+		b = 'hb; 
+		d = 'hd; 
+		e = 'he; 
+		c ='h0;
+
+		repeat (18)
+		begin
+			#5 c = (c+'h1);
+		end
 	
+		#5 -> second;
+	end
+
+	initial begin
+		@(second);
+		a = 'h1; 
+		b = 'h2; 
+		d = 'h3; 
+		e = 'h4; 
+		c ='h0;
+
+		repeat (8)
+		begin
+			#5 c = (c+'h1);
+		end
+	
+		#5 -> third;
+	end
+
+	initial begin
+		@(third);
+		a = 'h5; 
+		b = 'h6; 
+		d = 'h7; 
+		e = 'h8; 
+		c ='h0;
+
+		repeat (7)
+		begin
+			#5 c = (c+'h1);
+		end
+		
+		#5 
+		$display ("###################################################");
+		#1 
 		$finish();
 	end
 
 	initial begin
-		$monitor("t=%-4d: a = %d, b = %d, c = %d, d = %d, q = %d", $time, a, b, c, d, q);
+		$monitor("t=%-4d: a = %h, b = %h, c = %h, d = %h, e = %h, q = %h", $time, a, b, c, d, e, q);
 	end
 endmodule
